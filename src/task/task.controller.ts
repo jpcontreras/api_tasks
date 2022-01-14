d8868255affecd841a09f2b9acc04389f7826aa3
Juan Contreras <jpcontreras.dev@gmail.com>
2022-01-13 23:25:45 -0500

    CRUD of tasks in memory was created

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,85 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query, Req } from '@nestjs/common';
+import { Request } from 'express';
+import { TaskDTO } from './dto/task.dto';
+import { TaskService } from './task.service';
 
 @Controller('api/v1/task')
-export class TaskController {}
+export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+
+  @Post()
+  create(@Body() taskDTO: TaskDTO) {
+    return this.taskService.create(taskDTO);
+  }
+
+  @Get()
+  findAll() {
+    return this.taskService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.taskService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() taskDto: TaskDTO) {
+    return this.taskService.update(id, taskDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): string {
+    return this.taskService.delete(id);
+  }
+
+  /*
+    // Params Decorator
+    @Post(':id/description/:description/is-done/:isDone')
+    post(
+        @Param('id') id: number, 
+        @Param('description') description: string,
+        @Param('isDone') isDone: boolean
+    ){
+        return { id, description, isDone };
+    }
+
+    // Query Params Decorator
+    @Post()
+    post1(
+        @Query('id') id: number, 
+        @Query('description') description: string,
+        @Query('isDone') isDone: boolean
+    ){
+        return { id, description, isDone };
+    }
+
+    @Post()
+    post2(
+        @Body('id') id: number, 
+        @Body('description') description: string,
+        @Body('isDone') isDone: boolean
+    ){
+        return { id, description, isDone };
+    }
+
+    @Get('done')
+    get(@Req() req: Request){
+        return `method ${req.method}`;
+    }
+
+    @Put()
+    put(@Req() req: Request){
+        return `method ${req.method}`;
+    }
+
+    @Patch()
+    patch(@Req() req: Request){
+        return `method ${req.method}`;
+    }
+
+    @Delete()
+    delete(@Req() req: Request){
+        return `method ${req.method}`;
+    }
+    */
+}
